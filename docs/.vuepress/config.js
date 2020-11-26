02cbc7e84d01808b7c6ce90ecc6cc737a945b014
ozaki25 <fs65ugumori@yahoo.co.jp>
2020-11-26 16:46:14 +0900

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,48 @@
+const dayjs = require('dayjs');
+
+module.exports = {
+  title: 'HelloWorld',
+  themeConfig: {
+    domain: 'https://xxx.ozaki25.vercel.app',
+    repo: 'ozaki25/vuepress_template',
+    repoLabel: 'GitHub',
+    sidebar: [
+      {
+        title: 'TOP',
+        children: ['/'],
+      },
+      {
+        title: 'Subject1',
+        children: ['/subject1/page1', '/subject1/page2', '/subject1/page3'],
+      },
+      {
+        title: 'Subject2',
+        children: ['/subject2/page1', '/subject2/page2', '/subject2/page3'],
+      },
+      {
+        title: 'Subject3',
+        children: ['/subject3/page1', '/subject3/page2', '/subject3/page3'],
+      },
+    ],
+  },
+  markdown: {
+    lineNumbers: true,
+  },
+  plugins: {
+    '@vuepress/last-updated': {
+      transformer: (timestamp, lang) => {
+        return dayjs(timestamp).format('YYYY/MM/DD');
+      },
+    },
+    '@vuepress/back-to-top': {},
+    '@vuepress/medium-zoom': {},
+    '@vuepress/pwa': {
+      serviceWorker: true,
+      updatePopup: true,
+    },
+    seo: {
+      description: () => 'ハンズオン資料',
+    },
+  },
+  head: [['link', { rel: 'manifest', href: '/manifest.json' }]],
+};
